0446bbfc0b2b2eeedf17ae82d1330dbeab4523b1
Ismarji Workman <ismarjiw@protonmail.com>
2022-11-07 12:57:10 -0500

    first commit

---
+++ wordcount.py
@@ -0,0 +1,32 @@
+"""Count words in file."""
+
+
+# put your code here.
+text = open('test.txt')
+
+word_count = {}
+
+all_words = []
+
+for line in text:
+    line = line.rstrip()
+    words = line.split(' ')
+    all_words.extend(words)
+
+for word in all_words:
+    if word in word_count:
+        word_count[word] += 1 
+    else:
+        word_count[word] = 1
+#     word_count[word] = word_count.get(word, 0) + 1
+
+print(word_count)
+
+
+
+
+
+
+
+
+
